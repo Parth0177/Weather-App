0e2a1d3d787d39d3cd539dd0cc1488dddc0f290c
Parth Tiwari <parthtiwari670@gmail.com>
2025-06-14 01:21:30 +0530

    Initialize weather application with React and Vite
    - Created package.json with dependencies and scripts for development.
    - Added Vite logo SVG to public directory.
    - Created App component and associated CSS.
    - Added weather-related assets (icons) to the assets directory.
    - Implemented Weather component with search functionality and weather data display.
    - Styled components with CSS for a visually appealing layout.
    - Set up main entry point for the application.
    - Configured Vite for React support.

---
+++ src/components/Weather.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import './Weather.css'
+import search_icon from '../assets/Assets/search.png'
+import clear from '../assets/Assets/clear.png'
+import cloud from '../assets/Assets/cloud.png'
+import drizzle from '../assets/Assets/drizzle.png'
+import humidity from '../assets/Assets/humidity.png'
+import rain from '../assets/Assets/rain.png'
+import snow from '../assets/Assets/snow.png'
+import wind from '../assets/Assets/wind.png'
+
+
+const Weather = () => {
+  return (
+    <div className='weather'>
+      <div className="search">
+        <input type="text" placeholder='Search' />
+        <img src={search_icon} alt="" />
+      </div>
+      <img src={clear} alt="" className='weather_icon' />
+      <p className='temperature'>16&#8451;</p>
+      <p className='city'>London</p>
+      <div className="weather-data">
+        <div className="col">
+          <img src={humidity} alt="" />
+          <div>
+            <p>91 %</p>
+            <span>Humidity</span>
+          </div>
+        </div>
+        <div className="col">
+          <img src={wind} alt="" />
+          <div>
+            <p>3.6 km/hr</p>
+            <span>Wind Speed</span>
+          </div>
+        </div>
+        <div className="col">
+          <img src={humidity} alt="" />
+          <div>
+            <p>91 %</p>
+            <span>Humidity</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Weather
